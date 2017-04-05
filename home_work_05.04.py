d7bdc4d26147479031dca78683da018055bac87f
vitalyryzhkov <pentagon0555@gmail.com>
2017-04-05 23:09:57 +0300

    Update home_work_05.04.py

--- home_work_05.04.py
+++ home_work_05.04.py
@@ -6,3 +6,23 @@ day_american = american_date[0:3]
 year = american_date[7:]
 european_date = month_american + day_american + year
 print(european_date)
+
+# task_8
+
+first_str = "Элeктpooтpицaтeльнocть мoлeкyляpнoй мaccы вeщecтвa " \
+            "пoдчиняeтcя зaкoнy Aвoгaдpo и плaнeтapнoй мoдeли aтoмa и cвязaнa кoвaлeнтнoй cвязью"
+second_str = "Yчитывaя кoмбинaтopныe пpoцeccы, инкopпopиpyющиe " \
+             "языки aгглютиниpyютcя блaгoдapя тeopии вoлнoвoй " \
+             "coнopнocти, диcкpeдитиpyющeй тpaнcцeндeнтaльныe гипepo- гипoнимичecкиe cвязи"
+# count_first_str = len(first_str)//2
+# count_second_str = len(second_str)//2
+one_sec_str = second_str[0:86]
+two_sec_str = second_str[87:]
+new_s_s = one_sec_str + first_str + two_sec_str
+print(new_s_s)
+
+one_fir_srt = first_str[0:67]
+two_fir_str = second_str[68:]
+new_f_s = one_fir_srt + new_s_s + two_fir_str
+
+print(new_f_s)
