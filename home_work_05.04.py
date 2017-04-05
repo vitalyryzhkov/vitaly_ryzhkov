ad73d367812f5c3baec1b48fba8136921bad1734
vitalyryzhkov <pentagon0555@gmail.com>
2017-04-05 22:40:02 +0300

    Create home_work_05.04.py

---
+++ home_work_05.04.py
@@ -0,0 +1,8 @@
+#task_7
+
+american_date = "05.17.2016"
+month_american = american_date[3:6]
+day_american = american_date[0:3]
+year = american_date[7:]
+european_date = month_american + day_american + year
+print(european_date)
