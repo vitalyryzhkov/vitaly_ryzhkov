f9e3c053219bdc21bc62ede4f56aa2986b4c12bc
Vitaliy <pentagon0555@gmail.com>
2017-04-14 23:40:58 +0300

    task_17

--- home_work_14_17.py
+++ home_work_14_17.py
@@ -1,14 +1,13 @@
 # task_14
 
-# def is_even(number):
-#     result = number % 2
-#     if result == 0:
-#         result = "Число является четным"
-#     else:
-#         result = "Число является нечетным"
-#     return result
-#
-# print(is_even(int(input("Введите число:"))))
+def is_even(number):
+    result = number % 2
+    if result == 0:
+        print("Число четное")
+    else:
+        print("Число нечетное")
+    return result
+(is_even(int(input("Введите число:"))))
 
 
 # task_15
@@ -18,7 +17,7 @@
 # task_17
 from math import *
 
-def is_quadratic(a, b, c):
+def root_square(a, b, c):
     diskr = pow(b, 2) - 4 * a * c
     if diskr > 0:
         x1 = (-b-sqrt(diskr))/(2*a)
@@ -26,12 +25,12 @@ def is_quadratic(a, b, c):
     elif diskr == 0:
         x1 = x2 = (-b)/(2*a)
     elif diskr < 0:
-        x1 = x2 = "none"
+        x1 = x2 = None
     return x1, x2
 
 a = int(input("Введите первый коэффициент квадратоного уравнения: "))
 b = int(input("Введите второй коэффициент квадратоного уравнения: "))
 c = int(input("Введите свободный член квадратоного уравнения: "))
 print("Результат вычисления квадратного уравнения при a = %d, b = %d и c = %d равен: "
-      % (a, b, c), is_quadratic(a, b, c))
+      % (a, b, c), root_square(a, b, c))
 
