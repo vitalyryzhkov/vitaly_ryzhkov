09022df604bedd270f135cfc0979cd7553468386
Vitaliy <pentagon0555@gmail.com>
2017-04-15 15:36:17 +0300

    С 15 заданием мне нужна помошь))

--- home_work_14_17.py
+++ home_work_14_17.py
@@ -1,5 +1,5 @@
 # task_14
-
+"""
 def is_even(number):
     result = number % 2
     return result == 0
@@ -9,9 +9,30 @@ def is_even(number):
     #     return False
 # print(type(is_even(5)))
 print("Number is even?:", is_even(int(input("Enter any number: "))))
-
+"""
 
 # task_15
+from math import *
+
+x1 = 1
+x2 = 8
+y1 = 2
+y2 = 7
+r1 = 1
+r2 = 1
+
+def is_circles_intersect():
+    distance_between_radii = sqrt(pow((x2-x1), 2)+pow((y2-y1), 2))
+    if distance_between_radii <= (r1+r2):
+        print("Circles is intersect")
+    if x1 == x2 and y1 == y2 and r1 == r2:
+        print("Окружности одинаковы")
+    else:
+        print("Окружности не пересекаются")
+        return distance_between_radii
+
+is_circles_intersect()
+
 # task_16
 
 # task_17
