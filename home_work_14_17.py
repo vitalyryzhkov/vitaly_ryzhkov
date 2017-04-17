6874ad82af1beea0a1fe590005547bc983b41cc2
Vitaliy <pentagon0555@gmail.com>
2017-04-17 13:30:40 +0300

    Выполнил 16 задание

--- home_work_14_17.py
+++ home_work_14_17.py
@@ -12,22 +12,32 @@ print("Number is even?:", is_even(int(input("Enter any number: "))))
 """
 
 # task_15
-from math import *
-
-x1 = 1
-x2 = 2
-y1 = 8
-y2 = 14
-r1 = 4
-r2 = 7
-
-def is_circles_intersect():
-    distance_between_centers = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
-    result = distance_between_centers - (r1+r2)
-    return result <= 0
-print("Is circles intersect?:", is_circles_intersect())
+# from math import *
+#
+# x1 = 1
+# x2 = 2
+# y1 = 8
+# y2 = 14
+# r1 = 4
+# r2 = 7
+#
+# def is_circles_intersect():
+#     distance_between_centers = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
+#     result = distance_between_centers - (r1+r2)
+#     return result <= 0
+# print("Is circles intersect?:", is_circles_intersect())
 
 # task_16
+
+first_train_speed = 50
+second_tarin_speed = 70
+first_train_time = 4 / first_train_speed
+second_train_time = 6 / second_tarin_speed
+
+if first_train_time < second_train_time:
+    print("Поезда не столкнутся")
+else:
+    print("Поезда столкнутся")
 
 # task_17
 """
