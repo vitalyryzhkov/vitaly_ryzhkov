9c657e0a2b01b6f0a0aeb9e3728bb4d4e8f8f76e
Vitaliy <pentagon0555@gmail.com>
2017-04-20 19:25:38 +0300

    ))

--- home_work_18_20.py
+++ home_work_18_20.py
@@ -5,5 +5,5 @@ for x in range(1000000):
     x = x**3
     if x % 3 == 0:
         sum_of_all_numbers = sum_of_all_numbers + x
-print("Вывести сумму всех чисел, которые являются степенью 3ки от 0 до 1000000 равна:", sum_of_all_numbers)
+print("Сумма всех чисел, которые являются степенью 3ки от 0 до 1000000 равна:", sum_of_all_numbers)
 
