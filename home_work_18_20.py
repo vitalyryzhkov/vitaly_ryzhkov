74b200e3bb36ce9a0df0b4d43ea84088d5b99505
Vitaliy <pentagon0555@gmail.com>
2017-04-21 13:09:47 +0300

    №19

--- home_work_18_20.py
+++ home_work_18_20.py
@@ -1,21 +1,15 @@
 # task_19
 
-# sum_of_all_numbers = 0
-# for x in range(1000000):
-#     x = x**3
-#     if x % 3 == 0:
-#         sum_of_all_numbers = sum_of_all_numbers + x
-# print("Сумма всех чисел, которые являются степенью 3ки от 0 до 1000000 равна:", sum_of_all_numbers)
-# from math import *
-# for x in range(100):
-#     if (x**3) % 3 == 0:
-#         a = x**(1/3)
-#         sum_of_all_numbers = sum_of_all_numbers + ceil(a)
-# print(sum_of_all_numbers)
+import math
+sum_of_all_numbers = 0
+for x in range(int(math.pow(1000000, 1/3))+1):
+    x = x**3
+    # if x > 0 and x < 1000000:
+    sum_of_all_numbers = sum_of_all_numbers + x
+print("Сумма всех чисел, которые являются степенью 3ки от 0 до 1000000 равна:", sum_of_all_numbers)
 
 # task_20
 
 for x in range(1000):
-    if '1' in str(x):
-        if '7' in str(x):
-            print(x)
+    if '1' in str(x) and '7' in str(x):
+        print(x)
