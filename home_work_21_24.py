4309db7f1697f64ee2fc6b9e806e455c60dd01db
Vitaliy <pentagon0555@gmail.com>
2017-04-23 23:58:49 +0300

    №21

--- home_work_21_24.py
+++ home_work_21_24.py
@@ -1,11 +1,29 @@
-import random
+# import random
+#
+# def avr_random_numbers(num_randoms, lower_limit, upper_limit):
+#     avr_num = 0
+#     for i in range(num_randoms):
+#         num = random.randint(lower_limit, upper_limit)
+#         # print(num)
+#         avr_num = avr_num + num
+#     print("Среднее арифметическое =", avr_num/num_randoms)
+#
+# avr_random_numbers(10, 0, 10)
 
-def avr_random_numbers(num_randoms, lower_limit, upper_limit):
-    avr_num = 0
-    for i in range(num_randoms):
-        num = random.randint(lower_limit, upper_limit)
-        # print(num)
-        avr_num = avr_num + num
-    print("Среднее арифметическое =", avr_num/num_randoms)
+# task_21
 
-avr_random_numbers(10, 0, 10)
+def is_prime(x):
+    for i in range(1, x):
+        if i <= 2:
+            continue
+        if x % i == 0 or x % 2 == 0:
+            return False
+    else:
+        return True
+
+# print(isprime(11))
+
+for a in range(1, 101):
+    if is_prime(a):
+        print(a)
+
