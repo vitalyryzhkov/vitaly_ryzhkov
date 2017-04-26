a3532d4fa562ccac3da55a5bc1fbfd7842b5be81
Vitaliy <pentagon0555@gmail.com>
2017-04-26 23:40:43 +0300

    №25

--- home_work_25_30.py
+++ home_work_25_30.py
@@ -1,8 +1,23 @@
-planets = [' mercury', ' mars', ' earth', ' venus']
+# planets = [' mercury', ' mars', ' earth', ' venus']
+#
+# def normalize_name_of_planets(planets):
+#     for i, elem in enumerate(planets):
+#         planets[i] = i[1:]
+#     print(planets)
+#
+# normalize_name_of_planets(planets)
 
-def normalize_name_of_planets(planets):
-    for i, elem in enumerate(planets):
-        planets[i] = elem[1:]
-    print(planets)
+# task_25
 
-normalize_name_of_planets(planets)
+lst = [1, 2, 3, 4, 5]
+def avr_lst(lst):
+    sum_list_elem = 0
+    for i in range(len(lst)+1):
+        sum_list_elem += i
+    print(sum_list_elem/len(lst))
+avr_lst(lst)
+
+
+
+
+
