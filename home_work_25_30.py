6f45dfbf7ebc8c54edf6ce47e98fee150229d2bf
Vitaliy <pentagon0555@gmail.com>
2017-04-30 18:53:48 +0300

    №28

--- home_work_25_30.py
+++ home_work_25_30.py
@@ -49,20 +49,49 @@
 # task_28
 
 import random
-lst1 = []
-lst2 = []
-sum_lst1 = 0
-sum_lst2 = 0
-for i in range(5):
-    lst1.append(random.randint(0, 6))
-    lst2.append(random.randint(0, 6))
-for a in lst1:
-    sum_lst1 += a
-for c in lst2:
-    sum_lst2 += c
+# lst1 = []
+# lst2 = []
+# sum_lst1 = 0
+# sum_lst2 = 0
+# for i in range(5):
+#     lst1.append(random.randint(0, 6))
+#     lst2.append(random.randint(0, 6))
+# for a in lst1:
+#     sum_lst1 += a
+# for c in lst2:
+#     sum_lst2 += c
+#
+# avr_sum_lst1 = sum_lst1/len(lst1)
+# avr_sum_lst2 = sum_lst2/len(lst2)
 
-avr_sum_lst1 = sum_lst1/len(lst1)
-avr_sum_lst2 = sum_lst2/len(lst2)
+def create_list(num, lower_limit, upper_limit):
+    create_lst = []
+    for i in range(num):
+        create_lst.append(random.randint(lower_limit, upper_limit))
+    return create_lst
 
-print(lst1, sum_lst1, avr_sum_lst1)
-print(lst2, sum_lst2, avr_sum_lst2)
+def sum_list(lst):
+    sum_lst = 0
+    for i in lst:
+        sum_lst += i
+    return sum_lst
+
+lst1 = create_list(5, 0, 5)
+lst2 = create_list(5, 0, 5)
+sum_lst1 = sum_list(lst1)
+sum_lst2 = sum_list(lst2)
+avr_lst1 = sum_lst1/len(lst1)
+avr_lst2 = sum_lst2/len(lst2)
+
+print(lst1)
+print(lst2)
+if avr_lst1 > avr_lst2:
+    print("Среднее арифметическое первого списка больше")
+elif avr_lst1 < avr_lst2:
+    print("Среднее арифметическое второго списка больше")
+else:
+    print("Средние арифметические списков равны")
+
+# print(sum_lst1, avr_lst1)
+# print(sum_lst2, avr_lst2)
+# print(lst2, sum_lst2, avr_sum_lst2)
