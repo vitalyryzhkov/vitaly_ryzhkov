64b2c1f8578dab39adeb9bc1eb46e16b9c21770c
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-10 16:46:01 +0300

    Update home_work_25_30.py

--- home_work_25_30.py
+++ home_work_25_30.py
@@ -135,9 +135,9 @@ def encryption(input_string):
     lst_encryption_table = list(encryption_table)
     for i in range(len(input_string)):
         find_idx = encryption_table.find(input_string[i])
-        new_index = find_idx + 5
-        if new_index > len(encryption_table):
-            new_index = (find_idx + 5)%len(encryption_table)
+        new_index = (find_idx + 5)%len(encryption_table)
+#         if new_index > len(encryption_table):
+#             new_index = find_idx - len(encryption_table) + 5
         lst_password.append(lst_encryption_table[new_index])
     password = ''.join(lst_password)
     return password
