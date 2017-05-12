0c21ffbffd5e7f72e4282eb5eb22ce487c66b0da
Vitaliy <pentagon0555@gmail.com>
2017-05-12 21:46:29 +0300

    task 8

--- test_1.py
+++ test_1.py
@@ -58,7 +58,7 @@ while True:
         continue
 multiply(number)
 """
-
+"""
 # task 5 Создать программу, выводящую на экран ближайшее к 10 из
 # двух чисел, введенных пользователем. Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.
 
@@ -68,4 +68,19 @@ def nearest_number(main_num, first_num, second_num):
     else:
         print("Ближайшее число к основному - ", second_num)
 nearest_number(10, 8.5, 11.45)
+"""
 
+# task 8 В одномерном массиве поменять местами минимальный и максимальный элементы. Остальные оставить на своих местах.
+
+import random
+
+lst = []
+for i in range(10):
+    lst.append(random.randint(1, 10))
+min_elem = lst.index(min(lst))
+max_elem = lst.index(max(lst))
+print(lst)
+replace = lst[max_elem]
+lst[max_elem] = lst[min_elem]
+lst[min_elem] = replace
+print(lst)
