5fac070a3bf5d238806baa3c7069981dd3fb98a0
Vitaliy <pentagon0555@gmail.com>
2017-05-17 22:56:19 +0300

    не пойму почему у меня сортируются строки, а не столбцы, можете помочь?

--- test_1.py
+++ test_1.py
@@ -201,3 +201,27 @@ for i in range(3):
         new_table[j][i] = table[i][j]
 print_table(new_table)
 """
+import random
+
+def print_table(table):
+    for line in table:
+        for elem in line:
+            print(elem, end="\t")
+        print()
+
+table = [[0 for i in range(10)] for j in range(10)]
+
+table1 = []
+
+for i in range(len(table)):
+    for j in range(len(table[i])):
+        table[i][j] = random.randint(1, 101)
+print_table(table)
+print()
+
+for i in range(len(table)):
+    if table[i][0] % 2 == 0:
+        table[i].sort()
+    else:
+        table[i].sort(reverse=True)
+print_table(table)
