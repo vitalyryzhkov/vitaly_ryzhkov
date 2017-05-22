c1009b32adb5eb3b5d729a43f1e6c802e7b71909
Vitaliy <pentagon0555@gmail.com>
2017-05-23 00:08:55 +0300

    №33

---
+++ home_work_33.py
@@ -0,0 +1,131 @@
+group = [
+    {"id": 1024, "fullname": "Александр Скворцов",    "email": "", "github": "https://github.com/Agskvortsov", "rank": 0},
+    {"id": 1025, "fullname": "Андрей Рожко",          "email": "", "github": "https://github.com/rozhkoandrew", "rank": 0},
+    {"id": 1026, "fullname": "Кулишенко Алексей",     "email": "", "github": "https://github.com/", "rank": 0},
+    {"id": 1027, "fullname": "Виталий Рыжков",        "email": "", "github": "https://github.com/vitalyryzhkov", "rank": 0},
+    {"id": 1028, "fullname": "Гавеля Виталина",       "email": "", "github": "https://github.com/", "rank": 0},
+    {"id": 1029, "fullname": "Виктор Бурлаков",       "email": "", "github": "https://github.com/SancheeZzz", "rank": 0},
+    {"id": 1030, "fullname": "Виктор Горовой",        "email": "", "github": "https://github.com/lamboosanproject", "rank": 0},
+    {"id": 1031, "fullname": "Надежда Симанович",     "email": "", "github": "https://github.com/simanadya", "rank": 0},
+    {"id": 1032, "fullname": "Николай Марушевский",   "email": "", "github": "https://github.com/Gelios-Sky", "rank": 0},
+    {"id": 1033, "fullname": "Андрей Кравчук",        "email": "", "github": "https://github.com/albaderon", "rank": 0},
+    {"id": 1034, "fullname": "Шадрина Екатерина",     "email": "", "github": "https://github.com/katefeline", "rank": 0},
+    {"id": 1035, "fullname": "Александр Малышев",     "email": "", "github": "https://github.com/Barabasha", "rank": 0},
+    {"id": 1036, "fullname": "Владимир Веренчук",     "email": "", "github": "https://github.com/", "rank": 0}
+]
+
+hw_results = [
+    {"id": 1024, "task_completion": [1,1,1,1,1,1,1,0,1,0,1,0,0,1,0,1,1,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1025, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0] },
+    {"id": 1026, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1027, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1029, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0] },
+    {"id": 1030, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1031, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1032, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1034, "task_completion": [1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1035, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1036, "task_completion": [1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] }
+]
+
+test1_results = [
+    {"id": 1024, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1025, "task_completion": [1,1,1,1,1,1,1,1,1,0,0,1] },
+    {"id": 1026, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1027, "task_completion": [1,1,1,1,1,1,1,1,0,0,0,0] },
+    {"id": 1028, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1029, "task_completion": [1,1,1,1,1,0,0,0,0,0,0,0] },
+    {"id": 1030, "task_completion": [1,1,1,1,1,1,1,1,0,0,0,0] },
+    {"id": 1031, "task_completion": [1,1,1,1,0,0,0,0,0,0,0,0] },
+    {"id": 1032, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,0,0,1] },
+    {"id": 1034, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1035, "task_completion": [1,1,1,1,1,1,1,1,1,0,1,1] },
+    {"id": 1036, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] }
+]
+
+test1_weights = {
+    1 : 1,
+    2 : 1,
+    3 : 1,
+    4 : 2,
+    5 : 2,
+    6 : 2,
+    7 : 4,
+    8 : 4,
+    9 : 4,
+    10 : 8,
+    11 : 8,
+    12 : 15
+}
+
+
+def update_students_results(group):
+    '''
+    Calculate student results and put them into the student dictionary under the key "rank".
+    Total rank is calculated as a sum of completed hw tasks +
+        sum of completed Test1 tasks weighted proportional to its weights.
+    For example, student with id=1025 has total rank = 1*32 + (1*1 + 1*1 + 1*1 + ... 1*15) = 68)
+    :return: None
+    '''
+
+    for i in range(len(test1_results)):
+        for j in range(len(test1_weights)):
+            test1_results[i]['task_completion'][j] *= test1_weights[j + 1]
+
+    for elem in group:
+        for elem1 in hw_results:
+            if elem['id'] == elem1['id']:
+                elem['rank'] = sum(elem1['task_completion'])
+
+    for elem in group:
+        for elem1 in test1_results:
+            if elem['id'] == elem1['id']:
+                elem['rank'] += sum(elem1['task_completion'])
+    return group
+
+def pretty_print(group):
+
+    for i in group:
+
+        print('-' * 40)
+        print(": ID: %34s;" % i['id'])
+        print('-' * 40)
+        print(": Full name: %27s;" % i['fullname'])
+        print(": Email: %s" % i['email'])
+        print(": Giithub: %29s;" % i['github'][group[0]['github'].rindex('/') + 1:])
+        print(": Rank: %32s;" % i['rank'])
+        print('_' * 40)
+        # print('_' * 40)
+
+
+
+def print_students_info(group, sort_by_key="fullname"):
+    '''
+    Prints students info sorted according to the passed key in dictionary). By default, sort by students names.
+    Student info should be presented as a card that includes the following information:
+        - id,
+        - name,
+        - email,
+        - github account (only name, not URL path)
+        - rank
+    Example (format is not strictly required):
+        -----------------------------------------
+        : ID:                               1025:
+        :.......................................:
+        : Full name:          Александр Скворцов:
+        : Email:           agskvortsov@gmail.com:
+        : Github:                    Agskvortsov:
+        : Rank:                               42:
+        -----------------------------------------
+    :return: None
+    '''
+    group.sort(key=lambda d_group: d_group.get(sort_by_key))
+
+    pretty_print(group)
+
+if __name__ == "__main__":
+    update_students_results(group)
+    print_students_info(group, "rank")
