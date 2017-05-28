d97952b9433f6dd6116737b75981bea9ccd9d991
Vitaliy <pentagon0555@gmail.com>
2017-05-28 16:00:18 +0300

    вроде вышло

--- godzilla.py
+++ godzilla.py
@@ -2,14 +2,16 @@ from random import randint
 
 
 class Godzilla:
-    def __init__(self, volume_stomach, count_stomach_full, stomach_full):
+    CAPACITY_LIMIT = 0.9
+
+    def __init__(self, volume_stomach, count_stomach_full):
         self.volume_stomach = volume_stomach
-        self.stomach_full = stomach_full
+        # self.stomach_full = stomach_full
         self.count_stomach_full = count_stomach_full
 
     def is_hungry(self, people_weight):
         # count_stomach_full = 0
-        if people_weight < self.stomach_full - self.count_stomach_full:
+        if self.count_stomach_full < self.volume_stomach * Godzilla.CAPACITY_LIMIT:
             self.count_stomach_full += people_weight
             print("Godzilla ate", self.count_stomach_full, "kg, peoples meat.....")
             return False
@@ -19,5 +21,6 @@ class Godzilla:
 
 
 people_weight = 0
-while Godzilla(100, 0, 90).is_hungry(people_weight) == False:
+godzilla = Godzilla(100, 0)
+while godzilla.is_hungry(people_weight) == False:
     people_weight = randint(1, 15)
