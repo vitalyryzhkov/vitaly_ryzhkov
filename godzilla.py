ab97f5d8068d3c972ee33f522235e3ef7286366a
Vitaliy <pentagon0555@gmail.com>
2017-05-28 12:55:30 +0300

    №34

--- godzilla.py
+++ godzilla.py
@@ -1,21 +1,22 @@
+from random import randint
+
+
 class Godzilla:
     def __init__(self, volume_stomach):
         self.volume_stomach = volume_stomach
+        self.stomach_full = volume_stomach * 0.9
 
-    def eat_peoples(self):
-        from random import randint
-
-        stomach_full = self.volume_stomach * 0.9
+    def is_hungry(self, people_weight):
         count_stomach_full = 0
-
-        while count_stomach_full < int(stomach_full):
-            people_weight = randint(1, 95)
-            if people_weight > stomach_full - count_stomach_full:
-                continue
+        if people_weight < self.stomach_full - count_stomach_full:
             count_stomach_full += people_weight
             print("Godzilla ate", count_stomach_full, "kg, peoples meat.....")
+            return False
+        else:
+            print("Godzilla is full and can no longer eat people")
+            return True
 
-        print("Godzilla is full and can no longer eat people")
 
-
-Godzilla(1000).eat_peoples()
+people_weight = 0
+while Godzilla(100).is_hungry(people_weight) == False:
+    people_weight = randint(1, 95)
